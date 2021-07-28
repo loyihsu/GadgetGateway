8f192362aa250a0cd8f1b988c1883ac6fcf2a94a
Anamika Maurya <ana.maurya93@gmail.com>
2021-07-28 20:23:17 +0100

    added columns category and url in pages

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.db.models.query_utils import PathInfo
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,8 @@ from django.db.models.deletion import CASCADE
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -16,7 +18,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
